e52f832f3a3af8d53920688bc525734d5c12d218
Bin Yang <bin.yang@amlogic.com>
2019-12-03 19:33:03 +0800

    Reland "Add correct LOCAL_SHARED_LIBRARIES for amlogic"
    This reverts commit e35ee152a7e8021bb15dbe29553dbbb17c01cac1.
    
    Change-Id: Idad637e5887078e67c5b5e423af5d30ef4d3ad49
    Signed-off-by: Bin Yang <bin.yang@amlogic.com>

--- Android.mk
+++ Android.mk
@@ -5,8 +5,8 @@ LOCAL_MODULE_SUFFIX :=.so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRELINK_MODULE := false
-LOCAL_SHARED_LIBRARIES := libcurl
 LOCAL_PRODUCT_MODULE := true
+LOCAL_SHARED_LIBRARIES := libEGL libGLESv2 libandroid libbinder libcurl libft2 libgui libjpeg liblog libsqlite libssl libutils
 #directory
 DVBCORE_DIRECTORY := $(LOCAL_PATH)/../DVBCore/
 HAVE_DVBCORE_DIRECTORY := $(shell test -d $(DVBCORE_DIRECTORY) && echo yes)
