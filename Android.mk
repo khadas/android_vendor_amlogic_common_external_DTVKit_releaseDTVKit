383bb25a029c5207a81602fdafae5f00f1bd4cd7
junchao yuan <junchao.yuan@amlogic.com>
2020-05-29 14:08:51 +0800

    releaseDTVKit: release dtvkitserver to vendor [8/9]
    PD#SWPL-25745
    
    Problem:
    need to release dtvkitserver to vendor
    
    Solution:
    release dtvkitserver to vendor
    
    Verify:
    ac214
    
    Change-Id: I4304846419b6082e47e9a18f6f052642f36c1c2a
    Signed-off-by: junchao yuan <junchao.yuan@amlogic.com>

--- Android.mk
+++ Android.mk
@@ -1,22 +1,44 @@
 LOCAL_PATH := $(call my-dir)
+ifeq (,$(wildcard $(LOCAL_PATH)/../DVBCore))
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_SUFFIX :=.so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRELINK_MODULE := false
-LOCAL_PRODUCT_MODULE := true
+LOCAL_VENDOR_MODULE := true
 LOCAL_STRIP_MODULE := false
-LOCAL_SHARED_LIBRARIES := libEGL libGLESv2 libandroid libbinder libcurl libft2 libgui libjpeg liblog libsqlite libssl libutils
-#directory
-DVBCORE_DIRECTORY := $(LOCAL_PATH)/../DVBCore/
-HAVE_DVBCORE_DIRECTORY := $(shell test -d $(DVBCORE_DIRECTORY) && echo yes)
-ifeq ($(HAVE_DVBCORE_DIRECTORY),yes)
-$(warning "DVBCore directory exist")
-LOCAL_MODULE := libinvaildDTVKit
-LOCAL_SRC_FILES := libinvaildDTVKit.so
-else
-LOCAL_MODULE := libdtvkitserver
-LOCAL_SRC_FILES := libdtvkitserver.so
-endif
+LOCAL_SHARED_LIBRARIES := \
+    android.hidl.allocator@1.0 \
+    libam_adp \
+    libam_mw \
+    libamdvr \
+    libbase \
+    libbinder \
+    libc++ \
+    libc \
+    libcrypto \
+    libcurl \
+    libcutils \
+    libdl \
+    libft2-aml \
+    libhidlbase \
+    libhidlmemory \
+    libhidltransport \
+    libjpeg \
+    liblog \
+    libm \
+    libmediahal_resman \
+    libmediahal_tsplayer \
+    libmediandk \
+    libsqlite \
+    libssl \
+    libutils \
+    vendor.amlogic.hardware.dtvkitserver@1.0
+
+LOCAL_MODULE := dtvkitserver
+LOCAL_SRC_FILES := dtvkitserver
+LOCAL_INIT_RC := dtvkitserver.rc
+LOCAL_VINTF_FRAGMENTS := vendor.amlogic.hardware.dtvkitserver@1.0.xml
+LOCAL_MODULE_RELATIVE_PATH := hw
 include $(BUILD_PREBUILT)
+endif
